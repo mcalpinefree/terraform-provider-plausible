57f7159c1a8b8a9649e0c9fe2a6d6b201d00cac0
Kurt McAlpine <kurt@linux.com>
2020-11-13 00:33:18 +1300

    Implement plausible_shared_link resource

---
+++ internal/provider/resource_shared_link.go
@@ -0,0 +1,73 @@
+package provider
+
+import (
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/mcalpinefree/terraform-provider-plausible/plausibleclient"
+)
+
+func resourceSharedLink() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceSharedLinkCreate,
+		Read:   resourceSharedLinkRead,
+		Delete: resourceSharedLinkDelete,
+
+		Schema: map[string]*schema.Schema{
+			"site_id": {
+				Description: "The domain of the site to create the shared link for.",
+				Type:        schema.TypeString,
+				Required:    true,
+				ForceNew:    true,
+			},
+			"password": {
+				Description: "Add a password or leave it blank so anyone with the link can see the stats.",
+				Type:        schema.TypeString,
+				Optional:    true,
+				ForceNew:    true,
+			},
+			"link": {
+				Description: "Shared link",
+				Type:        schema.TypeString,
+				Computed:    true,
+			},
+		},
+	}
+}
+
+func resourceSharedLinkCreate(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*apiClient)
+	domain := d.Get("site_id").(string)
+	password := d.Get("password").(string)
+	sharedLink, err := client.plausibleClient.CreateSharedLink(domain, password)
+	if err != nil {
+		return err
+	}
+	d.SetId(sharedLink.ID)
+	return resourceSharedLinkSetResourceData(sharedLink, d)
+}
+
+func resourceSharedLinkSetResourceData(sharedLink *plausibleclient.SharedLink, d *schema.ResourceData) error {
+	d.Set("domain", sharedLink.Domain)
+	d.Set("password", sharedLink.Password)
+	d.Set("link", sharedLink.Link)
+	return nil
+}
+
+func resourceSharedLinkRead(d *schema.ResourceData, meta interface{}) error {
+	id := d.Id()
+
+	sharedLink := &plausibleclient.SharedLink{
+		ID:       id,
+		Domain:   d.Get("site_id").(string),
+		Password: d.Get("password").(string),
+		Link:     d.Get("link").(string),
+	}
+
+	return resourceSharedLinkSetResourceData(sharedLink, d)
+}
+
+func resourceSharedLinkDelete(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*apiClient)
+	id := d.Id()
+	domain := d.Get("site_id").(string)
+	return client.plausibleClient.DeleteSharedLink(domain, id)
+}
---
+++ plausibleclient/shared_link.go
@@ -0,0 +1,129 @@
+package plausibleclient
+
+import (
+	"fmt"
+	"log"
+	"net/url"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+type SharedLink struct {
+	ID       string
+	Domain   string
+	Password string
+	Link     string
+}
+
+func (c *Client) CreateSharedLink(domain, password string) (*SharedLink, error) {
+	if !c.loggedIn {
+		err := c.login()
+		if err != nil {
+			return nil, err
+		}
+	}
+	resp, err := c.httpClient.Get("https://plausible.io/sites/" + domain + "/shared-links/new")
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	// Load the HTML document
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	// Find the form CSRF token
+	csrfToken := ""
+	csrfTokenExists := false
+	doc.Find(`form > input[name="_csrf_token"]`).Each(func(i int, s *goquery.Selection) {
+		csrfToken, csrfTokenExists = s.Attr("value")
+	})
+	if !csrfTokenExists {
+		return nil, fmt.Errorf("could not find csrf token in HTML form")
+	}
+
+	// super hacky but the /shared-links POST does not return the ID of the
+	// shared link that was created so we have to look up the shared links
+	// before and after the request and compare to find out what was
+	// created
+	before, err := c.GetSiteSettings(domain)
+	if err != nil {
+		return nil, err
+	}
+
+	values := url.Values{}
+	values.Add("_csrf_token", csrfToken)
+	values.Add("shared_link[password]", password)
+	_, err = c.httpClient.PostForm("https://plausible.io/sites/"+domain+"/shared-links", values)
+	if err != nil {
+		return nil, err
+	}
+
+	after, err := c.GetSiteSettings(domain)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(before.SharedLinks) != (len(after.SharedLinks) - 1) {
+		return nil, fmt.Errorf("expected there to be one more shared link after requesting to create a new one, but the count went from %d to %d", len(before.SharedLinks), len(after.SharedLinks))
+	}
+
+AFTER:
+	for _, v := range after.SharedLinks {
+		for _, w := range before.SharedLinks {
+			if v == w {
+				continue AFTER
+			}
+		}
+		parts := strings.Split(v, "/")
+		return &SharedLink{
+			ID:       parts[len(parts)-1],
+			Domain:   domain,
+			Password: password,
+			Link:     v,
+		}, nil
+	}
+
+	return nil, fmt.Errorf("could not find newly created shared link")
+}
+
+func (c *Client) DeleteSharedLink(domain, id string) error {
+	if !c.loggedIn {
+		err := c.login()
+		if err != nil {
+			return err
+		}
+	}
+	resp, err := c.httpClient.Get("https://plausible.io/" + domain + "/settings")
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// Load the HTML document
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	// Find the form CSRF token
+	csrfToken := ""
+	csrfTokenExists := false
+	cssSelector := fmt.Sprintf(`button[data-to="/sites/%s/shared-links/%s"]`, domain, id)
+	log.Printf("[TRACE] cssSelector: %s\n", cssSelector)
+	doc.Find(cssSelector).Each(func(i int, s *goquery.Selection) {
+		csrfToken, csrfTokenExists = s.Attr("data-csrf")
+	})
+	if !csrfTokenExists {
+		return fmt.Errorf("could not find csrf token in HTML form")
+	}
+
+	values := url.Values{}
+	values.Add("_csrf_token", csrfToken)
+	values.Add("_method", "delete")
+	_, err = c.httpClient.PostForm("https://plausible.io/sites/"+domain+"/shared-links/"+id, values)
+	return err
+}
